f47fdca1cedc5a380675dd3ff1541679dbe230e0
Gknuts <gknuts@ulb.ac.be>
2016-05-01 04:49:56 +0200

    Unification

--- Projet_2/Gare.py
+++ Projet_2/Gare.py
@@ -2,14 +2,38 @@
 
 
 class Gare(object):
+    """
+    Constructeur
+    Paramètre:
+        - name: Le nom de la gare
+        - distance: La distance entre cette gare et son père
+        - gares_available: Gares désservie depuis celle-ci
+    attributs:
+        - name: Le nom de la gare
+        - distance: La distance entre cette gare et son père
+        - gares_available: Gares désservie depuis celle-ci
+    """
     def __init__(self, name, distance=0):
         self._name = name
         self._distance = distance
         self._gares_available = []
 
+    """
+    add_gare_available
+    Description:
+        Rajoute une nouvelle gare désservie
+    paramètre:
+        - sub_tree: la nouvelle gare
+    """
     def add_gare_available(self, sub_tree):
         self._gares_available.append(sub_tree)
 
+    """
+    gareAccessibles
+    Description:
+        Retourne la liste des gares désservie
+    paramètre:
+    """
     def gareAccessibles(self):
         temp = []
         for gare in self._gares_available:
--- Projet_2/projet2.py
+++ Projet_2/projet2.py
@@ -1,5 +1,58 @@
 # coding: utf8
-from Gare import Gare
+
+
+class Gare(object):
+    """
+    Constructeur
+    Paramètre:
+        - name: Le nom de la gare
+        - distance: La distance entre cette gare et son père
+        - gares_available: Gares désservie depuis celle-ci
+    attributs:
+        - name: Le nom de la gare
+        - distance: La distance entre cette gare et son père
+        - gares_available: Gares désservie depuis celle-ci
+    """
+    def __init__(self, name, distance=0):
+        self._name = name
+        self._distance = distance
+        self._gares_available = []
+
+    """
+    add_gare_available
+    Description:
+        Rajoute une nouvelle gare désservie
+    paramètre:
+        - sub_tree: la nouvelle gare
+    """
+    def add_gare_available(self, sub_tree):
+        self._gares_available.append(sub_tree)
+
+    """
+    gareAccessibles
+    Description:
+        Retourne la liste des gares désservie
+    paramètre:
+    """
+    def gareAccessibles(self):
+        temp = []
+        for gare in self._gares_available:
+            temp.append(gare._name)
+
+        return temp
+
+    def __str__(self):
+        msg = ""
+        msg += "======================================\n"
+        msg += "name: " + self._name
+        msg += "\n"
+        msg += "======================================\n"
+        for gare in self._gares_available:
+            msg += "gare: " + gare._name + " ,distance: " + gare._distance
+            msg += "\n"
+        msg += "======================================\n"
+        return msg
+
 
 
 class ReseauFerroviaire(object):
@@ -257,3 +310,140 @@ class ReseauFerroviaire(object):
             msg += str(gare)
             msg += "\n"
         return msg
+
+def match(parcour1, parcour2):
+    for a in parcour1:
+        for b in parcour2:
+            if a == b:
+                return a
+
+    return None
+
+def min_distance(chemins):
+    to_return = chemins[0]
+    max = chemins[0]["distance"]
+    for chemin in chemins:
+        if chemin["distance"] < max:
+            to_return = chemin
+            max = chemin["distance"]
+
+    return to_return
+
+def remove_after(parcour, ville):
+    # index = 0
+    # cpt = 0
+    # for elm in parcour:
+    #     if elm == ville:
+    #         index = cpt
+    #     cpt += 1
+
+    index = parcour.index(ville)
+    if index != 0:
+        temp = list(reversed(parcour))[:-index]
+        return list(reversed(temp))
+    else:
+        return parcour
+
+def remove_before(parcour, ville):
+    # index = 0
+    # cpt = 0
+    # for elm in parcour:
+    #     if elm == ville:
+    #         index = cpt
+    #     cpt += 1
+
+    index = parcour.index(ville)
+    if index != 0:
+        temp = list(reversed(parcour))[:-index]
+        return list(reversed(temp))
+    else:
+        return parcour
+
+def trouverParcoursMin(reseaux, villeA, villeB):
+    chemins = []
+    for reseau in reseaux:
+        accessible = reseau.garesAccessibles()
+        if villeA in accessible:
+            if villeB in accessible:
+                parcour = reseau.get_parcours(villeA, villeB)
+                if(parcour != []):
+                    distance = reseau.compute_distance(parcour)
+                    distance -= reseau.compute_distance_int(villeA)
+                    chemin = {
+                        "parcour": parcour,
+                        "distance": distance
+                    }
+                    chemins.append(chemin)
+            else:
+                for reseau2 in reseaux:
+                    if reseau2 != reseau:
+                        accessible2 = reseau2.garesAccessibles()
+                        if villeB in accessible2:
+                            middle = match(accessible, accessible2)
+                            if(middle != None):
+                                a = reseau.trouverParcours([middle])[-1]
+                                b = reseau2.trouverParcours([middle])[-1]
+                                c = None
+                                distance = 0
+                                if(villeA not in a):
+                                    c = reseau.trouverParcours([villeA])[-1]
+                                if c!= None:
+                                    distance += reseau.compute_distance(c)
+                                    c = list(reversed(c))
+                                    del c[-1]
+                                else:
+                                    a = remove_before(a, villeA)
+                                b = remove_after(b, villeB)
+                                distance += reseau.compute_distance(a)
+                                distance += reseau2.compute_distance(b)
+                                distance -= reseau2.compute_distance_int(b[0])
+                                del b[-1]
+                                b = list(reversed(b))
+                                a.extend(b)
+                                if c != None:
+                                    c.extend(a)
+                                    a = c
+                                chemin = {
+                                    "parcour": a,
+                                    "distance": distance
+                                }
+                                chemins.append(chemin)
+    return min_distance(chemins)
+
+
+def output(reponse):
+    if reponse == None:
+        print("Aucuns parcours possible")
+    else:
+        villeA = reponse["parcour"][0]
+        villeB = reponse["parcour"][-1]
+        print("Meilleur Parcours possible entre " + villeA + " et " + villeB + " :")
+        for elm in enumerate(reponse["parcour"]):
+            if(elm[0] != len(reponse["parcour"])-1):
+                print(elm[1], end=", ")
+            else:
+                print(elm[1])
+        print("Distance: " + str(reponse["distance"]))
+
+
+if __name__ == "__main__":
+    reseaux = []
+
+    reseau_bxl = ReseauFerroviaire()
+    reseau_bxl.init_data("Bruxelles.txt")
+    reseaux.append(reseau_bxl)
+
+    reseau_rome = ReseauFerroviaire()
+    reseau_rome.init_data("Rome.txt")
+    reseaux.append(reseau_rome)
+
+
+    output(trouverParcoursMin(reseaux, "Bruxelles", "Milan"))
+    output(trouverParcoursMin(reseaux, "Bruxelles", "Bologne"))
+    output(trouverParcoursMin(reseaux, "Bruxelles", "Florence"))
+    output(trouverParcoursMin(reseaux, "Bruxelles", "Rome"))
+    output(trouverParcoursMin(reseaux, "Lille", "Milan"))
+    output(trouverParcoursMin(reseaux, "Namur", "Rome"))
+    output(trouverParcoursMin(reseaux, "Mons", "Rome"))
+    pass
+
